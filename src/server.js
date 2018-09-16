9d1bb80ca33890ad25e444840f48759ac80b00aa
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-09-16 20:41:49 +0530

    Message and User Extraction Transfer done

--- src/server.js
+++ src/server.js
@@ -7,8 +7,15 @@ const utils = require('./modules/utils/utils');
 
 Commands that work in groups
 
+For User:-
+
 /tip @user <amount> - tip a user with specified token amount
 /upvote <amount> - reply to any message in your chat. Tips the creator with specified amount.
+
+For Admin:-
+
+/ban @user - ban a user from receiving tokens hence forth
+/unban @user - unban a user from receiving tokens
 
 Commands that work in DMs
 
@@ -18,8 +25,6 @@ For User:-
 
 For Admin:-
 
-/ban @user - ban a user from receiving tokens hence forth
-/unban @user - unban a user from receiving tokens
 /set_cycle <days> - sets redemption cycle in number of days
 /set_bounty <amount> - set bounty for the specified period
 /set_daily_award <amount> - sets daily award to be given to users
@@ -28,14 +33,21 @@ For Admin:-
 
 */
 
-var supergroupId = '-1001241579924';
+const SUPERGROUP_ID = '-1001241579924';
+const ALLOWED_COMMANDS = {
+  ADMIN: [
+    '/ban', '/unban', '/set_cycle', '/set_bounty',
+    '/set_daily_award', '/award', '/deduct'],
+  USER: [
+    '/tip', '/upvote', '/balance', '/redeem']
+}
 
 // :: Group based Commands ::
 
 // For User
 
 bot.onText(/\/start/, (message) => {
-  // Setup the supergroupId
+  // Setup the SUPERGROUP_ID
 });
 
 bot.onText(/\/help/, (message) => {
@@ -44,14 +56,13 @@ bot.onText(/\/help/, (message) => {
 
 bot.on('message', (message) => {
     // console.log(message);
-    bot.getChatAdministrators(supergroupId)
+    bot.getChatAdministrators(SUPERGROUP_ID)
     .then((admins) => {
       // console.log(data);
       const user = extractUser(message, admins);
-      // const user = extractUser(data); <-- Set properties like isAdmin, allowedCommands
-      // user's name, user's unique Id, isHeAdmin, listOfAllowedCommands
-
-
+      console.log('Extracted User =>', user);
+      const message_text = extractText(message);
+      console.log(message_text);
       // const isPrivateChat = message.chat.type == "private" ? true : false;
 
       // executeCommand(mode, command_name, data)
@@ -60,15 +71,28 @@ bot.on('message', (message) => {
 });
 
 const extractUser = function(message, admins) {
-  console.log('Sent Message ->', message);
-  console.log('Admins for Supergroup ->', admins);
-
+  //console.log('Sent Message ->', message);
+  //console.log('Admins for Supergroup ->', admins);
+  var id = message.from.id; var username = message.from.username;
+  // console.log(message.from.id);
+  var is_admin = false;
+  for(var i = 0; i < admins.length; i++) {
+    // console.log(admins);
+    var admin = admins[i].user;
+    // console.log("Current Admin", admin);
+    if(`${admin.id}` == `${id}`) {
+      is_admin = true;
+      break;
+    }
+  }
+  var allowed_commands = is_admin ? ALLOWED_COMMANDS.ADMIN : ALLOWED_COMMANDS.USER;
+  var user = { id, username, is_admin, allowed_commands};
+  return user;
 }
 
-// extractUser(user) {
-  // const isUserAdmin = check if user is amongst admins passed
-  // const allowedCommands = getAllowedCommands(user);
-//}
+const extractText = function(message) {
+  return message.text;
+}
 
 // executeCommand(mode, command_name, user) {
   // Check if command allowed or not by referring array, else respond kindly
